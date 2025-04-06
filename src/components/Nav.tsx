ea6b654ea0b218280f831296b98c1da17d49068e
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-04-05 23:23:15 -0300

    add lucide-solid dependency and update Nav component with icons and styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,22 +1,31 @@
 import { useLocation } from "@solidjs/router";
+import { Home, Info } from "lucide-solid";
 
 export default function Nav() {
   const location = useLocation();
+
   const active = (path: string) =>
     path == location.pathname
-      ? "border-blue-500"
-      : "border-transparent hover:border-blue-500";
+      ? "border-blue-200"
+      : "border-transparent hover:border-blue-200";
 
   return (
     <nav class="bg-blue-500">
-      <ul class="container flex items-center p-3 text-white">
-        <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-          <a href="/">Home</a>
-        </li>
-        <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-          <a href="/about">About</a>
-        </li>
-      </ul>
+      <div class="w-full flex items-center justify-between p-3 text-white">
+        <span class="text-2xl font-bold tracking-wide">Academia Unifor</span>
+        <ul class="flex items-center space-x-6">
+          <li class={`border-b-2 ${active("/")}`}>
+            <a href="/" class="flex items-center gap-1">
+              <Home size={18} /> Home
+            </a>
+          </li>
+          <li class={`border-b-2 ${active("/about")}`}>
+            <a href="/about" class="flex items-center gap-1">
+              <Info size={18} /> Sobre
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
