707b1b3b0ce3226c9f8e8dd0e20610bca4d72434
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-04-05 22:55:56 -0300

    update Nav component styling for improved visibility

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,10 +3,13 @@ import { useLocation } from "@solidjs/router";
 export default function Nav() {
   const location = useLocation();
   const active = (path: string) =>
-    path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
+    path == location.pathname
+      ? "border-blue-500"
+      : "border-transparent hover:border-blue-500";
+
   return (
-    <nav class="bg-sky-800">
-      <ul class="container flex items-center p-3 text-gray-200">
+    <nav class="bg-blue-500">
+      <ul class="container flex items-center p-3 text-white">
         <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
           <a href="/">Home</a>
         </li>
