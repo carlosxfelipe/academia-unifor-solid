ae06c83e9bceb4da55208d2d93b3a7d71fef8e80
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-04-05 22:54:51 -0300

    update About and Home pages with new styling and content

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -2,28 +2,28 @@ import { A } from "@solidjs/router";
 
 export default function About() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        About Page
-      </h1>
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <A href="/" class="text-sky-600 hover:underline">
-          Home
-        </A>
-        {" - "}
-        <span>About Page</span>
-      </p>
+    <main class="text-center mx-auto text-gray-700 p-4 bg-gray-100 min-h-screen">
+      <div class="p-12 bg-white rounded-xl shadow-md max-w-4xl mx-auto mt-16">
+        <h1 class="text-5xl text-blue-500 font-bold mb-8">Sobre o Projeto</h1>
+        <p class="mt-4 mb-8 text-gray-700">
+          Visite{" "}
+          <a
+            href="https://solidjs.com"
+            target="_blank"
+            class="text-blue-500 hover:underline"
+          >
+            solidjs.com
+          </a>{" "}
+          para aprender como construir apps com SolidJS.
+        </p>
+        <p class="my-4 text-gray-700">
+          <A href="/" class="text-blue-500 hover:underline">
+            Home
+          </A>
+          {" - "}
+          <span>About Page</span>
+        </p>
+      </div>
     </main>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,28 +2,28 @@ import { A } from "@solidjs/router";
 
 export default function Home() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello world!
-      </h1>
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
+    <main class="text-center mx-auto text-gray-700 p-4 bg-gray-100 min-h-screen">
+      <div class="p-12 bg-white rounded-xl shadow-md max-w-4xl mx-auto mt-16">
+        <h1 class="text-5xl text-blue-500 font-bold mb-8">Bem-vindo à Home!</h1>
+        <p class="mt-4 mb-8 text-gray-700">
+          Visite{" "}
+          <a
+            href="https://solidjs.com"
+            target="_blank"
+            class="text-blue-500 hover:underline"
+          >
+            solidjs.com
+          </a>{" "}
+          para aprender como construir apps com SolidJS.
+        </p>
+        <p class="my-4 text-gray-700">
+          <span>Home</span>
+          {" - "}
+          <A href="/about" class="text-blue-500 hover:underline">
+            About Page
+          </A>{" "}
+        </p>
+      </div>
     </main>
   );
 }
