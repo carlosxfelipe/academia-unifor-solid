1740b1136c1cc482d1a6385131b7652334c6b5ef
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-04-05 23:29:19 -0300

    update About and Home pages with new layout and content

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,27 +1,13 @@
-import { A } from "@solidjs/router";
-
 export default function About() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4 bg-gray-100 min-h-screen">
-      <div class="p-12 bg-white rounded-xl shadow-md max-w-4xl mx-auto mt-16">
-        <h1 class="text-5xl text-blue-500 font-bold mb-8">Sobre o Projeto</h1>
-        <p class="mt-4 mb-8 text-gray-700">
-          Visite{" "}
-          <a
-            href="https://solidjs.com"
-            target="_blank"
-            class="text-blue-500 hover:underline"
-          >
-            solidjs.com
-          </a>{" "}
-          para aprender como construir apps com SolidJS.
-        </p>
-        <p class="my-4 text-gray-700">
-          <A href="/" class="text-blue-500 hover:underline">
-            Home
-          </A>
-          {" - "}
-          <span>About Page</span>
+    <main class="text-gray-700 p-4 bg-gray-100 min-h-screen flex justify-center">
+      <div class="p-12 bg-white rounded-xl shadow-md max-w-7xl w-full mx-auto mt-4">
+        <h1 class="text-5xl text-blue-500 font-bold mb-8 text-left">
+          Bem-vindo a Sobre!
+        </h1>
+
+        <p class="mt-4 mb-8 text-gray-700 text-left">
+          Nisi minim et anim sint qui et proident cillum.
         </p>
       </div>
     </main>
