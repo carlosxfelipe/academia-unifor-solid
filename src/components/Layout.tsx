f7462680c05b8d3ab552621a43a094767a515a40
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-04-10 19:38:29 -0300

    add fluid prop to Layout component in EquipmentsPage for responsive design

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,14 +4,30 @@ import Footer from "./Footer";
 
 interface LayoutProps {
   children: any;
+  fluid?: boolean; // desliga apenas os estilos de largura máxima
+  unstyled?: boolean; // desliga todos os estilos
 }
 
 const Layout: Component<LayoutProps> = (props) => {
+  if (props.unstyled) {
+    return (
+      <>
+        <Navbar />
+        {props.children}
+        <Footer />
+      </>
+    );
+  }
+
   return (
     <>
       <Navbar />
       <main class="text-gray-700 dark:text-gray-200 p-4 bg-gray-100 dark:bg-gray-900 min-h-screen flex justify-center">
-        <div class="p-12 bg-white dark:bg-gray-800 rounded-xl shadow-md max-w-7xl w-full mx-auto mt-4">
+        <div
+          class={`p-12 bg-white dark:bg-gray-800 rounded-xl shadow-md ${
+            props.fluid ? "" : "max-w-7xl w-full mx-auto"
+          } mt-4`}
+        >
           {props.children}
         </div>
       </main>
--- src/routes/equipments.tsx
+++ src/routes/equipments.tsx
@@ -18,7 +18,7 @@ export default function EquipmentsPage() {
   });
 
   return (
-    <Layout>
+    <Layout fluid>
       <div class="p-4">
         <h1 class="text-2xl font-bold mb-4">Equipamentos</h1>
         <input
