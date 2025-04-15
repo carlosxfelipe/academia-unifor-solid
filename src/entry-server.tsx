cb6fd526a02ef78f508fab6e5c65d16557267f93
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-04-15 18:21:32 -0300

    update language attribute to Portuguese and add theme color and title in entry-server

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -4,11 +4,13 @@ import { createHandler, StartServer } from "@solidjs/start/server";
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
-      <html lang="en">
+      <html lang="pt-BR">
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#1D4ED8" />
           <link rel="icon" href="/favicon.ico" />
+          <title>Academia Unifor</title>
           {assets}
         </head>
         <body>
